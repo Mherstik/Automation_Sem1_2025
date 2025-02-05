619bf32e1d5311fb7db396636eecbed5ae5008d2
Marcus Herstik <marcus@herstik.com>
2025-02-05 12:04:15 +1000

    Starting GitHub

---
+++ Week 2 - part1.py
@@ -0,0 +1,34 @@
+# Author: Marcus
+# Date: 2025/02/05
+# 
+######
+# Ask a persons name
+# print("What is your name?", end = "" )
+# name = input()
+name = ""
+age = 0
+
+
+name = input("What is your name? ")
+
+
+# says hello to the person with their name.
+# print("Hi there " + name + ". Would you like a coffee?")
+print(f"Hi there {name}. Would you like's a coffee?")
+
+age = input(f"How old are you {name} ?")
+a = "45"    # This is a string
+b = 45      # This is an interger
+c = 45.0    # This is a float
+
+print(type(age))
+print(len(c))
+print(len(age))
+# age = int(age)
+# print(type(age))
+
+print(f'Next year you will"s be {int(age) + 1}!')
+# no validation
+
+
+
---
+++ Week1-Part1.py
@@ -0,0 +1,11 @@
+# ask their name & store as variable
+name = input("Gimme a name: ")
+
+print(name)
+
+#say hello and their name
+# using +
+print("Hello " + name)
+# ,
+print("Hello" , name)
+# f string aka string formating
