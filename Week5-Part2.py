3ba4e04b906f73354a5f11c2842ffc8bcffa7020
Marcus Herstik <marcus@herstik.com>
2025-02-26 12:59:36 +1000

    Update Week5-Part2.py

--- Week5-Part2.py
+++ Week5-Part2.py
@@ -4,23 +4,41 @@
 ###
 
 # Get a number
-userInput = ''
+def getNumber():
+    userInput = ''
 
-while userInput.isdigit() == False:
-    userInput = input ("Give me a number between 1 & 100: ")
-#     if int(userInput) > 100 or int(userInput) < 1:
-#         print("Please make it an integer between 1 and 100!")
-    try:
-        userInput = int(userInput)
-        if userInput > 100 or userInput < 1:
-            print("Between 1 and 100!")
-            userInput = ''
-            # continue
-        else:
-            break
-    except ValueError:
-        print("Please make it an integer between 1 and 100!")
-    except:
-        Print("Something went wrong")
-#     
-print(userInput)
+    while userInput.isdigit() == False:
+        userInput = input ("Give me a number between 1 & 100: ")
+    #     if int(userInput) > 100 or int(userInput) < 1:
+    #         print("Please make it an integer between 1 and 100!")
+        try:
+            userInput = int(userInput)
+            if userInput > 100 or userInput < 1:
+                print("Between 1 and 100!")
+                userInput = ''
+                # continue
+            else:
+                break
+        except ValueError:
+            print("Please make it an integer between 1 and 100!")
+        except:
+            Print("Something went wrong")
+         
+    print(userInput)
+    return userInput
+
+userList = []
+while len(userList) < 3:
+    print(len(userList))
+    userInput = getNumber()
+    userList.append(userInput)
+    print(userList)
+
+
+
+
+
+
+
+
+
