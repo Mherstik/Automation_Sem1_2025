ea93a907b21e6e1aad24c2dc55fd2b417c37d9f7
Marcus Herstik <marcus@herstik.com>
2025-02-26 10:34:46 +1000

    Create Week4.py

---
+++ Week4.py
@@ -0,0 +1,25 @@
+# # Input: Get 3 numbers
+# # Output: largest number
+# 
+# a = int(input("First number: "))
+# b = int(input("Second number: "))
+# c = int(input("Third number: "))
+#         
+# # assign largest to first number
+# largest = a
+# 
+# # check if first bigger than second
+# if b > a:
+#     largest = b
+# # check largest bigger than third
+# if c > largest:
+#     largest = c
+#     
+# print(largest)
+
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1
+    
+print("We are finished")
