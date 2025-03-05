5b92e9a3494bde53d420622f76bdaab7c5505b53
Marcus Herstik <marcus@herstik.com>
2025-03-05 10:58:07 +1000

    Update Wk6

--- Week6.py
+++ Week6.py
@@ -1,10 +1,47 @@
 ### do a divide by something
 
 a = 12
-b = input("Gimme a number: ")
 
 
 ### do tests for errors
-b = int(b)
-print(a / b)
+while True:
+    b = input("Gimme a number: ")
+    try:
+        b = int(b)
+        print(a / b)
+        break
+    except ValueError:
+        print("You gave an incorrect value.\nTry again")
+        
+    except ZeroDivisionError:
+        print("You gave me zero.\nTry again")
+## 
+print("It finished")
+##
 
+## Print error
+# x = 5
+# y = 0
+# z = x/y
+# print("Division", c)
+
+## Assertion
+
+password = "SuperSecret"
+password1 = "SuperSecret1"
+password2 = "SuperSecret22"
+
+if password == "SuperSecret":
+    print("Password incorrect")
+
+
+# assert password == 'SuperSecret', 'password should be different0'
+# assert password1 == 'SuperSecret1', 'password should be different1'
+# assert password2 == 'SuperSecret2', 'password should be different2'
+
+## Logging warning
+
+import logging
+x = 7
+y = 0
+logging.warning("This is an error")
---
+++ debug.py
@@ -0,0 +1,6 @@
+def debugger(x, y):
+    breakpoint()
+    result = x / y
+    return result
+
+print(debugger(5, 0))
