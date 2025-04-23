52cb206135ebd4ffa3676c0c5ea7e4b0026603d8
Marcus Herstik <marcus@herstik.com>
2025-04-23 10:33:47 +1000

    Create T2Wk1.py

---
+++ T2Wk1.py
@@ -0,0 +1,23 @@
+#!
+# Author: Marcus Herstik
+# Date: 20250423
+# Oi idiot... read this!!
+import os
+import sys
+import time
+
+# print(os.path.dirname(sys.executable))
+# print(sys.executable)
+
+## What is a speedtest??
+## How to get download speed??
+# Start a stopwatch = get the current time
+# Download a file
+# Stop the stopwatch
+# How long did it take = finish time - start time.
+# Calculate time vs file size.
+start = time.time()
+time.sleep(2)
+end = time.time()
+print(end - start)
+
