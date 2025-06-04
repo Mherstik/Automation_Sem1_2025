19a4c2410a5ebececd6d5e7980dedb27b7824441
Marcus Herstik <marcus@herstik.com>
2025-06-04 10:30:17 +1000

    Update T2Wk7-VU23215.py

--- T2Wk7-VU23215.py
+++ T2Wk7-VU23215.py
@@ -11,13 +11,20 @@ import os
 
 ## check what operating system I'm using
 import platform
-print(platform.platform())
-print(platform.machine())
-print(platform.processor())
+# print(platform.platform())
+# print(platform.machine())
+# print(platform.processor())
 print(platform.system())
-print(platform.release())
-print(platform.version())
-print(platform.system_alias())
+# print(platform.release())
+# print(platform.version())
+#### print(platform.system_alias()) ## DEAD!!
+osType = platform.system()
+pingType = ''
+if osType == 'Windows':
+    pingType = 'n'
+else:
+    pingType = 'c'
+
 
 network = input ("Enter first 3 numbers of IP network, e.g. 1.2.3: ")
 print(network)
@@ -26,5 +33,5 @@ print(network)
 for host in range (1, 254 + 1):
     print("Pinging " + network + "." + str(host))
 # Windows error here!!
-    os.system("ping -c 2 " + network + "." + str(host))
+    os.system(f"ping -{pingType} 2 " + network + "." + str(host))
 
